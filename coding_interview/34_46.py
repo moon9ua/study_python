f2cab8058e0a91f6f6cab9fd9b8555ae7d4e4399
moon9ua <koom6@naver.com>
2020-09-06 19:32:44 +0900

    20.09.06

---
+++ coding_interview/34_46.py
@@ -0,0 +1,15 @@
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+
+#         def dfs():
+#             tmp_elems = nums # 복사? 참조?
+#             elems = nums
+#             for elem in prev_elems:
+#                 elems
+
+test = [1, 2, 3, 4]
+test.append("a")
+print (test)
+test.pop()
+print (test)
